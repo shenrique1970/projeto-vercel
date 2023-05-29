f3e3a5ba3cfc786762fa924c271c4790c1f6ff92
Shenrique1970 <shvribeiro@hotmail.com>
2023-05-29 08:44:11 -0300

    projeto react

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+
+import styles from './title.module.css'
+
+
+export default function App() {
+
+  const [tarefas, setTarefas] = useState([])
+
+  useEffect(() =>{
+
+    async function buscarDados() {
+      fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(res => res.json())
+      .then(res => setTarefas(res))
+    }
+
+    buscarDados()
+
+  }, [])
+
+  return (
+    <div>
+      <h1 className={styles.titulo}>Deploy na vercel</h1>
+      <div>
+        {tarefas.map((tarefa) => {
+          return (
+            <div className="task-item" key={tarefa.id}>
+              <p> {tarefa.id} - {tarefa.tille} </p>
+              
+            </div>
+          )
+          })}
+
+      </div>
+    </div>
+    
+  )
+}
+
